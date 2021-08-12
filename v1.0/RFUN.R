b9e05cfee0d264619891dd9453a0aa4c7387ac42
zhangyi-he <jefferyhe@outlook.com>
2021-08-12 23:40:06 +0100

    Update RFUN.R

--- v1.0/RFUN.R
+++ v1.0/RFUN.R
@@ -5,7 +5,8 @@
 #' Phenotypes controlled by a single gene
 #' Constant natural selection and constant demographic histories
 
-#' Genotype frequency data
+#' Input: called genotypes
+#' Output: posteriors for the selection coefficient
 
 #' R functions
 
@@ -30,7 +31,7 @@ library("compiler")
 #enableJIT(1)
 
 # call C++ functions
-sourceCpp("./Code/Code v1.0/Code v1.0/CFUN.cpp")
+sourceCpp("./CFUN.cpp")
 
 ################################################################################
 
